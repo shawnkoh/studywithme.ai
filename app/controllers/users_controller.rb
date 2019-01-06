925287aaa4e81677ccb7764441b6f0f5fa066620
Shawn Koh <shawnkoh@me.com>
2019-01-06 20:31:30 +0800

    Finish User sign-up and added usernames

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,27 @@
 class UsersController < ApplicationController
-  def new
+  def show
+    @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  # Signup form issues a HTTP POST request to /users URl which is handled by users#create
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to studywithme.ai!"
+      redirect_to @user
+      # Handle a successful save
+    else
+      render 'new'
+    end
+  end
+
+  private
+    # Prevent user from submitting more params than they are supposed to through /signup
+    def user_params
+      params.require(:user).permit(:username, :email, :password, :password_confirmation, :name)
+    end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,16 +2,30 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password")
+    @user = User.new(username: "username", name: "Example User", email: "user@example.com", password: "password", password_confirmation: "password")
   end
   
   test "should be valid" do
     assert @user.valid?
   end
 
-  test "name should be present" do
-    @user.name = "        "
+  test "username should be present" do
+    @user.username = "     "
     assert_not @user.valid?
+  end
+
+  test "username should be unique" do
+    duplicate_user = @user.dup
+    duplicate_user.username = @user.username.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+  
+  test "username should be saved as lower-case" do
+    mixed_case_username = "UsErNaMe"
+    @user.username = mixed_case_username
+    @user.save
+    assert_equal mixed_case_username.downcase, @user.reload.username
   end
 
   test "email should be present" do
@@ -19,12 +33,7 @@ class UserTest < ActiveSupport::TestCase
     assert_not @user.valid?
   end
 
-  test "name should not be too long" do
-    @user.name = "a" * 51
-    assert_not @user.valid?
-  end
-
-  test "emnail should not be too long" do
+  test "email should not be too long" do
     @user.email = "a"* 244 + "@example.com"
     assert_not @user.valid?
   end
@@ -66,6 +75,16 @@ class UserTest < ActiveSupport::TestCase
 
   test "password should have a minimum length" do
     @user.password = @user.password_confirmation = "a" * 7
+    assert_not @user.valid?
+  end
+
+  test "name should be present" do
+    @user.name = "        "
+    assert_not @user.valid?
+  end
+
+  test "name should not be too long" do
+    @user.name = "a" * 51
     assert_not @user.valid?
   end
 end
